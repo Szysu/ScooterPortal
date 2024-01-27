01467f51352135aeecdb597bff95ad39744ff853
Szysu <szysu1@gmail.com>
2024-01-27 23:43:16 +0100

    .NET Aspire initial

---
+++ src/ScooterPortal.AppHost/Program.cs
@@ -0,0 +1,8 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var apiservice = builder.AddProject<Projects.ScooterPortal_ApiService>("apiservice");
+
+builder.AddProject<Projects.ScooterPortal_Web>("webfrontend")
+    .WithReference(apiservice);
+
+builder.Build().Run();
